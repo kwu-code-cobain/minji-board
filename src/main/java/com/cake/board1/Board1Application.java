285ec9a4e74bbb629833e19b64aa0f3a52a56b30
cakefeelsgood <bloom_vk@naver.com>
2024-07-10 03:04:17 +0900

    first commit

--- src/main/java/com/cake/board1/Board1Application.java
+++ src/main/java/com/cake/board1/Board1Application.java
@@ -7,7 +7,6 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class) // Spring Security 인증 기능 제외
-// Spring Security의 일부기능을 제외시킴 (시큐리티 적용할때 다시 해제할거임)
 public class Board1Application {
 
     public static void main(String[] args) {
