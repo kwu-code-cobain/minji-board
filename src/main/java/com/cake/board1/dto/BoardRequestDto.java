c51a11bc7b9088b7d78dcd58e14cec2c0e0ae4ad
cakefeelsgood <bloom_vk@naver.com>
2024-07-25 01:20:36 +0900

    fourth

--- src/main/java/com/cake/board1/dto/BoardRequestDto.java
+++ src/main/java/com/cake/board1/dto/BoardRequestDto.java
@@ -3,14 +3,13 @@ package com.cake.board1.dto;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 
-
-@AllArgsConstructor // 생성자
-@Getter // Getter 어노테이션 사용시 클래스의 모든 필드에 대한 getter 메소드 자동 생성됨
+@AllArgsConstructor
+@Getter
 public class BoardRequestDto {
-    private String name;  // 입력한 이름
-    private String title;  // 입력한 제목
-    private String contents; // 작성 내용
-    private String password; // 입력한 비밀번호
+    private String name;
+    private String title;
+    private String contents;
+    private String password;
 
 }
 
--- src/main/java/com/cake/board1/dto/BoardResponseDto.java
+++ src/main/java/com/cake/board1/dto/BoardResponseDto.java
@@ -2,16 +2,10 @@ package com.cake.board1.dto;
 
 import com.cake.board1.domain.Board;
 import lombok.Getter;
-import lombok.Setter;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
-/*
-  게시글 조회시, 클라이언트에게 게시글의 응답 데이터를 전송하기위한 dto를 정의한 것이다.
-  dto는 애플리케이션의 여러 레이어 간에 데이터를 전송하기 위해 사용되는 객체이다.
-  특히 서버에서 클라이언트로 데이터를 전달할 때 사용된다.
- */
 
 @Getter
 public class BoardResponseDto {
@@ -32,12 +26,14 @@ public class BoardResponseDto {
         this.modifiedAt= board.getModifiedAt();
     }
 
-    public BoardResponseDto(List<Board> boards) {
-        this.id = boards.get(0).getId();
-        this.title = boards.get(0).getTitle();
-        this.name = boards.get(0).getName();
-        this.contents = boards.get(0).getContents();
-        this.createAt = boards.get(0).getCreatedAt();
-        this.modifiedAt = boards.get(0).getModifiedAt();
+    public BoardResponseDto(List<Board> board) {
+        this.id = board.get(0).getId();
+        this.title = board.get(0).getTitle();
+        this.name = board.get(0).getName();
+        this.contents = board.get(0).getContents();
+        this.createAt = board.get(0).getCreatedAt();
+        this.modifiedAt = board.get(0).getModifiedAt();
     }
+
+
 }
